f39882316c6d7624a370b205258145961d9d3b6f
kreso90 <kresimir.selendic@vivelacar.com>
2025-05-16 17:01:41 +0200

    auth route fix

--- src/app/api/facility/[id]/route.ts
+++ src/app/api/facility/[id]/route.ts
@@ -1,7 +1,7 @@
 import { prisma } from "@/lib/prisma";
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../../auth/[...nextauth]/route";
+import { authOptions } from "@/lib/authOptions";
 
 export async function GET(req: Request) {
     const url = req.url.split("/");
